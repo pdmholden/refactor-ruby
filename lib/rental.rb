b38c96b8afb01ed86201a8dfd7b1c4c5ce86a3bd
Paul Holden <pdmholden@gmail.com>
2018-03-11 20:40:56 -0700

    Initial version of formatted files for Chapter 1

---
+++ lib/rental.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+class Rental
+  attr_reader :movie, :days_rented
+  def initialize(movie, days_rented)
+    @movie = movie
+    @days_rented = days_rented
+  end
+end
