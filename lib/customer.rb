37e1cd925796aca5c9184ea5e505054616e11702
Paul Holden <pdmholden@gmail.com>
2018-04-10 12:45:40 -0700

    Move total points calculation into its own looping method

--- lib/customer.rb
+++ lib/customer.rb
@@ -2,6 +2,7 @@
 
 class Customer
   attr_reader :name
+
   def initialize(name)
     @name = name
     @rentals = []
@@ -17,12 +18,11 @@ class Customer
     @rentals.each do |rental|
       # show figures for this rental
       result += "\t" + rental.movie.title + "\t" + rental.cost.to_s + "\n"
-
-      frequent_renter_points += rental.points
     end
+
     # add footer lines
     result += "Amount owed is #{total_cost}\n"
-    result += "You earned #{frequent_renter_points} frequent renter points"
+    result += "You earned #{total_points} frequent renter points"
     result
   end
 
@@ -32,6 +32,14 @@ class Customer
       total_amount += rental.cost
     end
     total_amount
+  end
+
+  def total_points
+    points = 0
+    @rentals.each do |rental|
+      points += rental.points
+    end
+    points
   end
 
 end
