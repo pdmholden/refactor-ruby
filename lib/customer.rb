0057fa9a7b6a8f3c938b07cc750931c17c4296f8
Paul Holden <pdmholden@gmail.com>
2018-04-10 12:43:45 -0700

    Move total_amount calculation into its own looping method

--- lib/customer.rb
+++ lib/customer.rb
@@ -12,20 +12,26 @@ class Customer
   end
 
   def statement
-    total_amount = 0
     frequent_renter_points = 0
     result = "Rental Record for #{@name}\n"
     @rentals.each do |rental|
       # show figures for this rental
       result += "\t" + rental.movie.title + "\t" + rental.cost.to_s + "\n"
 
-      total_amount += rental.cost
       frequent_renter_points += rental.points
     end
     # add footer lines
-    result += "Amount owed is #{total_amount}\n"
+    result += "Amount owed is #{total_cost}\n"
     result += "You earned #{frequent_renter_points} frequent renter points"
     result
+  end
+
+  def total_cost
+    total_amount = 0
+    @rentals.each do |rental|
+      total_amount += rental.cost
+    end
+    total_amount
   end
 
 end
