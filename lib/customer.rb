81679c96b151bdcaff1a04bb043160b6bf1c2529
Paul Holden <pdmholden@gmail.com>
2018-04-10 12:22:57 -0700

    Move frequent_renter_points logic to Rental

--- lib/customer.rb
+++ lib/customer.rb
@@ -16,12 +16,8 @@ class Customer
     frequent_renter_points = 0
     result = "Rental Record for #{@name}\n"
     @rentals.each do |rental|
-      # add frequent renter points
-      frequent_renter_points += 1
-      # add bonus for a two day new release rental
-      if rental.movie.price_code == Movie::NEW_RELEASE && rental.days_rented > 1
-        frequent_renter_points += 1
-      end
+      frequent_renter_points += rental.points
+
       # show figures for this rental
       result += "\t" + rental.movie.title + "\t" + rental.cost.to_s + "\n"
       total_amount += rental.cost
